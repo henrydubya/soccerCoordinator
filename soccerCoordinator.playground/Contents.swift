2c40eb8b6d4afcc9d8935322771be2a51506509e
Henry Williams <henryndubya@gmail.com>
2017-10-18 07:51:48 -0700

    adding project files

---
+++ soccerCoordinator.playground/Contents.swift
@@ -0,0 +1,145 @@
+// Each player is represnted by a dictionary with string keys and values
+let playerOne = [
+    "name" : "Joe Smith",
+    "height" : "42",
+    "experience" : "yes",
+    "gueardian" : "Jim and Jan Smith"
+    ]
+
+let playerTwo = [
+    "name" : "Jill Tanner",
+    "height" : "36",
+    "experience" : "yes",
+    "gueardian" : "Clara Tanner"
+    ]
+
+let playerThree = [
+    "name" : "Bill Bon",
+    "height" : "43",
+    "experience" : "yes",
+    "gueardian" : "Sara and Jenny Bon"
+    ]
+
+var playerFour = [
+    "name" : "Eva Gordon",
+    "height" : "45",
+    "experience" : "no",
+    "gueardian" : "Wendy and Mike Gordon"
+    ]
+
+let playerFive = [
+    "name" : "Matt Gill",
+    "height" : "40",
+    "experience" : "no",
+    "gueardian" : "Charles and Sylvia Gill"
+    ]
+
+var playerSix = [
+    "name" : "Kimmy Stein",
+    "height" : "41",
+    "experience" : "no",
+    "guardian" : "Bill and Hillary Stein"
+]
+
+let playerSeven = [
+    "name" : "Sammy Adams",
+    "height" : "45",
+    "experience" : "no",
+    "guardian" : "Jeff Adams"
+]
+
+let playerEight = [
+    "name" : "Karl Saygan",
+    "height" : "42",
+    "experience" : "yes",
+    "guardian" : "Heather Bledsoe"
+]
+
+let playerNine = [
+    "name" : "Suzanne Greenberg",
+    "height" : "44",
+    "experience" : "yes",
+    "guardian" : "Henrietta Dumas"
+]
+
+let playerTen = [
+    "name" : "Sal Dali",
+    "height" : "41",
+    "experience" : "no",
+    "guardian" : "Gala Dali"
+]
+
+let playerEleven = [
+    "name" : "Joe Kavalier",
+    "height" : "39",
+    "experience" : "no",
+    "guardian" : "Sam and Elaine Kavalier"
+]
+
+let playerTwelve = [
+    "name" : "Ben Finkelstein",
+    "height" : "44",
+    "experience" : "no",
+    "guardian" : "Aaron and Jill Finkelstein"
+]
+
+let playerThirteen = [
+    "name" : "Diego Soto",
+    "height" : "41",
+    "experience" : "yes",
+    "guardian" : "Robin and Sarika Soto"
+]
+
+let playerFourteen = [
+    "name" : "Chloe Alaska",
+    "height" : "47",
+    "experience" : "no",
+    "guardian" : "David and Jamie Alaska"
+]
+
+let playerFifteen = [
+    "name" : "Arnold Willis",
+    "height" : "43",
+    "experience" : "no",
+    "guardian" : "Claire Willis"
+]
+
+let playerSixteen = [
+    "name" : "Phillip Helm",
+    "height" : "44",
+    "experience" : "yes",
+    "guardian" : "Thomas Helm and Eva Jones"
+]
+
+let playerSeventeen = [
+    "name" : "Les Clay",
+    "height" : "42",
+    "experience" : "yes",
+    "guardian" : "Wynonna Brown"
+]
+
+let playerEighteen = [
+        "name" : "Herschel Krustofski",
+        "height" : "45",
+        "experience" : "yes",
+        "guardian" : "Hyman and Rachel Krustofski"
+    ]
+
+// Create a single collection (array, dictionary) that contains all 18 players
+let playerList = [playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, playerNine, playerTen, playerEleven, playerTwelve, playerThirteen, playerFourteen, playerFifteen, playerSixteen, playerSeventeen, playerEighteen]
+
+
+// Create variables that will hold all the players for the three teams using the team "name" prefixed with 'team'
+
+var teamSharks =
+
+var teamDragons =
+
+var teamRaptors =
+
+// Logic that prints a letter to the parent specifying player’s "name", "guardian" "name"s, team "name", and date/time of their first team practice
+
+
+// Store the letter in a collection variable "name"d letters
+
+var letters =
