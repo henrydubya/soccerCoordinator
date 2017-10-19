fb74ff0cc9dce20cfc1b12bc58380ab6783e6522
Henry Williams <henryndubya@gmail.com>
2017-10-19 09:18:17 -0700

    Aligning with offline copy for updates to dictionary types, creation of variables for team with types Added variable for experience to use in later switch statement Commented out ideas for later functions to keep working doc clean of errors

--- soccerCoordinator.playground/Contents.swift
+++ soccerCoordinator.playground/Contents.swift
@@ -1,145 +1,176 @@
 // Each player is represnted by a dictionary with string keys and values
-let playerOne = [
+let playerOne: [String: Any] = [
     "name" : "Joe Smith",
     "height" : "42",
-    "experience" : "yes",
+    "experience" : "true",
     "gueardian" : "Jim and Jan Smith"
-    ]
+]
 
-let playerTwo = [
+let playerTwo: [String: Any] = [
     "name" : "Jill Tanner",
     "height" : "36",
-    "experience" : "yes",
+    "experience" : "true",
     "gueardian" : "Clara Tanner"
-    ]
+]
 
-let playerThree = [
+let playerThree: [String: Any] = [
     "name" : "Bill Bon",
     "height" : "43",
-    "experience" : "yes",
+    "experience" : "true",
     "gueardian" : "Sara and Jenny Bon"
-    ]
+]
 
-var playerFour = [
+var playerFour: [String: Any] = [
     "name" : "Eva Gordon",
     "height" : "45",
-    "experience" : "no",
+    "experience" : "false",
     "gueardian" : "Wendy and Mike Gordon"
-    ]
+]
 
-let playerFive = [
+let playerFive: [String: Any] = [
     "name" : "Matt Gill",
     "height" : "40",
-    "experience" : "no",
+    "experience" : "false",
     "gueardian" : "Charles and Sylvia Gill"
-    ]
+]
 
-var playerSix = [
+var playerSix: [String: Any] = [
     "name" : "Kimmy Stein",
     "height" : "41",
-    "experience" : "no",
+    "experience" : "false",
     "guardian" : "Bill and Hillary Stein"
 ]
 
-let playerSeven = [
+let playerSeven: [String: Any] = [
     "name" : "Sammy Adams",
     "height" : "45",
-    "experience" : "no",
+    "experience" : "false",
     "guardian" : "Jeff Adams"
 ]
 
-let playerEight = [
+let playerEight: [String: Any] = [
     "name" : "Karl Saygan",
     "height" : "42",
-    "experience" : "yes",
+    "experience" : "true",
     "guardian" : "Heather Bledsoe"
 ]
 
-let playerNine = [
+let playerNine: [String: Any] = [
     "name" : "Suzanne Greenberg",
     "height" : "44",
-    "experience" : "yes",
+    "experience" : "true",
     "guardian" : "Henrietta Dumas"
 ]
 
-let playerTen = [
+let playerTen: [String: Any] = [
     "name" : "Sal Dali",
     "height" : "41",
-    "experience" : "no",
+    "experience" : "false",
     "guardian" : "Gala Dali"
 ]
 
-let playerEleven = [
+let playerEleven: [String: Any] = [
     "name" : "Joe Kavalier",
     "height" : "39",
-    "experience" : "no",
+    "experience" : "false",
     "guardian" : "Sam and Elaine Kavalier"
 ]
 
-let playerTwelve = [
+let playerTwelve: [String: Any] = [
     "name" : "Ben Finkelstein",
     "height" : "44",
-    "experience" : "no",
+    "experience" : "false",
     "guardian" : "Aaron and Jill Finkelstein"
 ]
 
-let playerThirteen = [
+let playerThirteen: [String: Any] = [
     "name" : "Diego Soto",
     "height" : "41",
-    "experience" : "yes",
+    "experience" : "true",
     "guardian" : "Robin and Sarika Soto"
 ]
 
-let playerFourteen = [
+let playerFourteen: [String: Any] = [
     "name" : "Chloe Alaska",
     "height" : "47",
-    "experience" : "no",
+    "experience" : "false",
     "guardian" : "David and Jamie Alaska"
 ]
 
-let playerFifteen = [
+let playerFifteen: [String: Any] = [
     "name" : "Arnold Willis",
     "height" : "43",
-    "experience" : "no",
+    "experience" : "false",
     "guardian" : "Claire Willis"
 ]
 
-let playerSixteen = [
+let playerSixteen: [String: Any] = [
     "name" : "Phillip Helm",
     "height" : "44",
-    "experience" : "yes",
+    "experience" : "true",
     "guardian" : "Thomas Helm and Eva Jones"
 ]
 
-let playerSeventeen = [
+let playerSeventeen: [String: Any] = [
     "name" : "Les Clay",
     "height" : "42",
-    "experience" : "yes",
+    "experience" : "true",
     "guardian" : "Wynonna Brown"
 ]
 
-let playerEighteen = [
-        "name" : "Herschel Krustofski",
-        "height" : "45",
-        "experience" : "yes",
-        "guardian" : "Hyman and Rachel Krustofski"
-    ]
+let playerEighteen: [String: Any] = [
+    "name" : "Herschel Krustofski",
+    "height" : "45",
+    "experience" : "true",
+    "guardian" : "Hyman and Rachel Krustofski"
+]
 
-// Create a single collection (array, dictionary) that contains all 18 players
-let playerList = [playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, playerNine, playerTen, playerEleven, playerTwelve, playerThirteen, playerFourteen, playerFifteen, playerSixteen, playerSeventeen, playerEighteen]
+// Single collection containing all players
+let players = [
+    playerOne,
+    playerTwo,
+    playerThree,
+    playerFour,
+    playerFive,
+    playerSix,
+    playerSeven,
+    playerEight,
+    playerNine,
+    playerTen,
+    playerEleven,
+    playerTwelve,
+    playerThirteen,
+    playerFourteen,
+    playerFifteen,
+    playerSixteen,
+    playerSeventeen,
+    playerEighteen
+]
 
 
 // Create variables that will hold all the players for the three teams using the team "name" prefixed with 'team'
 
-var teamSharks =
+var teamDragons: [String:Any] = [:]
+var teamRaptors: [String:Any] = [:]
+var teamSharks: [String:Any] = [:]
 
-var teamDragons =
+// Creating variables on which I'll use to switch and create teams
 
-var teamRaptors =
+var hasExperience: [String:Any] = [:]
+var hasNoExperience: [String:Any] = [:]
+
+/* Use a switch statement to determine the teams.
+ Create logic to iterate through all 18 players and assign them to teams such that the number of experienced players on each team are the same. Store each team’s players in its own new collection variable for use in Part 3. (Please note: your logic should work correctly regardless of the initial ordering of the players and should work, if we theoretically had more or less than 18 players, so NO MAGIC NUMBERS!)
+ 
+ Also, if you would like to attain an “exceeds expectations” rating for this project, add logic to ensure that each team's average height is within 1.5 inches of the others.
+ */
+
+// possibly if players()
 
 // Logic that prints a letter to the parent specifying player’s "name", "guardian" "name"s, team "name", and date/time of their first team practice
 
 
 // Store the letter in a collection variable "name"d letters
 
-var letters =
+// possilby var letters = [something]
+
