d927b6dc86d35db494fcb0c10226c5579ddec827
Henry Williams <rocknrollah@me.com>
2017-12-04 18:20:14 -0800

    Changes to sorting logic

--- soccerCoordinator.playground/Contents.swift
+++ soccerCoordinator.playground/Contents.swift
@@ -2,131 +2,131 @@
 let playerOne: [String: Any] = [
     "name" : "Joe Smith",
     "height" : "42.0",
-    "experience" : "true",
+    "experience" : true,
     "guardian" : "Jim and Jan Smith"
 ]
 
 let playerTwo: [String: Any] = [
     "name" : "Jill Tanner",
     "height" : "36.0",
-    "experience" : "true",
+    "experience" : true,
     "guardian" : "Clara Tanner"
 ]
 
 let playerThree: [String: Any] = [
     "name" : "Bill Bon",
     "height" : "43.0",
-    "experience" : "true",
+    "experience" : true,
     "guardian" : "Sara and Jenny Bon"
 ]
 
 var playerFour: [String: Any] = [
     "name" : "Eva Gordon",
     "height" : "45.0",
-    "experience" : "false",
+    "experience" : false,
     "guardian" : "Wendy and Mike Gordon"
 ]
 
 let playerFive: [String: Any] = [
     "name" : "Matt Gill",
     "height" : "40.0",
-    "experience" : "false",
+    "experience" : false,
     "guardian" : "Charles and Sylvia Gill"
 ]
 
 var playerSix: [String: Any] = [
     "name" : "Kimmy Stein",
     "height" : "41.0",
-    "experience" : "false",
+    "experience" : false,
     "guardian" : "Bill and Hillary Stein"
 ]
 
 let playerSeven: [String: Any] = [
     "name" : "Sammy Adams",
     "height" : "45.0",
-    "experience" : "false",
+    "experience" : false,
     "guardian" : "Jeff Adams"
 ]
 
 let playerEight: [String: Any] = [
     "name" : "Karl Saygan",
     "height" : "42.0",
-    "experience" : "true",
+    "experience" : true,
     "guardian" : "Heather Bledsoe"
 ]
 
 let playerNine: [String: Any] = [
     "name" : "Suzanne Greenberg",
     "height" : "44.0",
-    "experience" : "true",
+    "experience" : true,
     "guardian" : "Henrietta Dumas"
 ]
 
 let playerTen: [String: Any] = [
     "name" : "Sal Dali",
     "height" : "41.0",
-    "experience" : "false",
+    "experience" : false,
     "guardian" : "Gala Dali"
 ]
 
 let playerEleven: [String: Any] = [
     "name" : "Joe Kavalier",
     "height" : "39.0",
-    "experience" : "false",
+    "experience" : false,
     "guardian" : "Sam and Elaine Kavalier"
 ]
 
 let playerTwelve: [String: Any] = [
     "name" : "Ben Finkelstein",
     "height" : "44.0",
-    "experience" : "false",
+    "experience" : false,
     "guardian" : "Aaron and Jill Finkelstein"
 ]
 
 let playerThirteen: [String: Any] = [
     "name" : "Diego Soto",
     "height" : "41.0",
-    "experience" : "true",
+    "experience" : true,
     "guardian" : "Robin and Sarika Soto"
 ]
 
 let playerFourteen: [String: Any] = [
     "name" : "Chloe Alaska",
     "height" : "47.0",
-    "experience" : "false",
+    "experience" : false,
     "guardian" : "David and Jamie Alaska"
 ]
 
 let playerFifteen: [String: Any] = [
     "name" : "Arnold Willis",
     "height" : "43.0",
-    "experience" : "false",
+    "experience" : false,
     "guardian" : "Claire Willis"
 ]
 
 let playerSixteen: [String: Any] = [
     "name" : "Phillip Helm",
     "height" : "44",
-    "experience" : "true",
+    "experience" : true,
     "guardian" : "Thomas Helm and Eva Jones"
 ]
 
 let playerSeventeen: [String: Any] = [
     "name" : "Les Clay",
     "height" : "42.0",
-    "experience" : "true",
+    "experience" : true,
     "guardian" : "Wynonna Brown"
 ]
 
 let playerEighteen: [String: Any] = [
     "name" : "Herschel Krustofski",
     "height" : "45.0",
-    "experience" : "true",
+    "experience" : true,
     "guardian" : "Hyman and Rachel Krustofski"
 ]
 
 // Single collection containing all players
-let players = [
+var players = [
     playerOne,
     playerTwo,
     playerThree,
@@ -149,8 +149,8 @@ let players = [
 
 // Creating variables on which I'll use to switch and create teams
 
-var hasExperience: [String:Any] = [:]
-var hasNoExperience: [String:Any] = [:]
+var hasExperience = [[String:Any]]()
+var hasNoExperience = [[String:Any]]()
 
 /* Use a switch statement to determine the teams.
  Create logic to iterate through all 18 players and assign them to teams such that the number of experienced players on each team are the same. Store each team’s players in its own new collection variable for use in Part 3. (Please note: your logic should work correctly regardless of the initial ordering of the players and should work, if we theoretically had more or less than 18 players, so NO MAGIC NUMBERS!)
@@ -159,10 +159,11 @@ var hasNoExperience: [String:Any] = [:]
  */
 
 for player in players {
-    if player["experience"] as! Bool == true {
+    let isExperienced = player["experience"] as! Bool
+    
+    if isExperienced {
         hasExperience.append(player)
-    }
-    else {
+    } else {
         hasNoExperience.append(player)
     }
 }
@@ -202,6 +203,18 @@ for i in 0..<hasNoExperience.count {
         break
     }
 }
+
+// Make an array containing all letters
+var letters = [String]()
+
+// Iterate through Dragons list, append message to letters
+
+// Iterate through Sharks list
+
+// Iterate through Raptors list
+
+
+/*
 // Establishing all team's practice time and dates
 
 let firstPractice: [String: String] = ["Team Dragons": "March 17, 1pm", "Team Sharks": "March 17, 3pm", "Team Raptors": "March 18, 1pm"]
@@ -213,7 +226,7 @@ var letters: [String] = []
 // Letter logic based on player information
 
 func welcomeLetter(for player: [String:Any], onTeam team: String) -> String {
-    let letter = // Letter text goes here
+    let letter = ""
     return letter
 }
 
@@ -221,7 +234,7 @@ func welcomeLetter(for player: [String:Any], onTeam team: String) -> String {
 
 func addLetter(for team: [[String:Any]], onTeam: String) {
     for player in team {
-        letters.append(for: player, onTeam)
+        //letters.append(for: player, onTeam)
     }
 }
 
@@ -234,6 +247,6 @@ addLetter(for: teamRaptors, onTeam: "Team Raptors")
 for letter in letters {
     print(letter)
 }
-
+*/
 
 
